ee99ae3518df45a308efbd4564fefe0322688e36
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2017-07-30 23:36:50 -0300

    JSON criado

--- CrawlerNoticias/test/br/com/crawler/noticia/NoticiaTeste.java
+++ CrawlerNoticias/test/br/com/crawler/noticia/NoticiaTeste.java
@@ -1,6 +1,6 @@
 package br.com.crawler.noticia;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -16,14 +16,15 @@ public class NoticiaTeste {
 				"  <title>Título notícia</title>\n" +
 				"  <description>Descrição notícia</description>\n" +
 				"  <link>link.html</link>\n" +
+				"  <creator>Da redação de Auto Esporte</creator>\n" +
+				"  <guid>1551131</guid>\n" +
 				"</item>";
 				
-		Noticia noticias = new Noticia("Título notícia", "Descrição notícia", "link.html");
+		Noticia noticias = new Noticia("Título notícia", "Descrição notícia", "link.html", "Da redação de Auto Esporte", "1551131");
 		
 		XStream xstream = new XStream();
 		xstream.alias("item", Noticia.class);
 		String xmlGerado = xstream.toXML(noticias);
-		
 		assertEquals(xmlEsperado, xmlGerado);
 	}
 }
